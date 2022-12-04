ce003524f11fe133c49bc18470a47eb1d89d51a1
jonasgrunert <jonas@grunert-berlin.de>
2022-12-04 12:42:53 +0100

    Day 4

---
+++ 04.ts
@@ -0,0 +1,23 @@
+import Solution from "./_util.ts";
+
+const task = new Solution(
+  (arr: number[][]) =>
+    arr.filter(([f, s, t, l]) => (f <= t && s >= l) || (t <= f && l >= s))
+      .length,
+  (arr: number[][]) =>
+    arr.filter(([f, s, t, l]) => (f <= t && s >= t) || (t <= f && l >= f))
+      .length,
+  {
+    transform: (e) =>
+      e
+        .split(",")
+        .flatMap((s) => s.split("-"))
+        .map((n) => Number.parseInt(n)),
+    sep: "\n",
+  },
+);
+task.expect(2, 4);
+
+if (import.meta.main) await task.execute();
+
+export default task;
