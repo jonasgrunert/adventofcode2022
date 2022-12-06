9bdffc1938d10e4455fbafbdc03557f8cd34c1d4
jonasgrunert <jonas@grunert-berlin.de>
2022-12-06 20:24:44 +0100

    Day 6

---
+++ 06.ts
@@ -0,0 +1,18 @@
+import Solution from "./_util.ts";
+
+const marker = (n: number) => (arr: string[]) => {
+  let s: string[] = [];
+  for (let i = 0; i < arr[0].length; i++) {
+    const idx = s.findIndex((c) => c == arr[0][i]);
+    if (idx !== undefined) s = s.slice(idx + 1);
+    s.push(arr[0][i]);
+    if (s.length === n) return i + 1;
+  }
+};
+
+const task = new Solution(marker(4), marker(14));
+task.expect(7, 19);
+
+if (import.meta.main) await task.execute();
+
+export default task;
